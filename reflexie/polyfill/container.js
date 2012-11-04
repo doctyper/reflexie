c2b8bc504572b2a960b6336fab7298dfc4735fdf
Richard Herrera <rich@doctyper.com>
2012-11-04 15:23:45 -0800

    Polyfill attempt using position as engine.

--- reflexie/polyfill/container.js
+++ reflexie/polyfill/container.js
@@ -20,15 +20,54 @@ define([
 			return (isFlex && isDirection);
 		},
 
+		storePositionValues : function (items) {
+			var i, j, items;
+
+			for (i = 0, j = items.length; i < j; i++) {
+				item = items[i];
+				item.rect = item.element.getBoundingClientRect();
+			}
+
+			return items;
+		},
+
+		applyPositioning : function (container, items) {
+			var i, j, items, key,
+				increment = 0;
+
+			container.element.style.position = "relative";
+
+			for (i = 0, j = items.length; i < j; i++) {
+				item = items[i];
+				item.element.style.position = "absolute";
+
+				item.rect.top = items[0].rect.top;
+				item.rect.bottom = items[0].rect.bottom;
+				item.rect.left = increment;
+
+				for (key in item.rect) {
+					item.element.style[key] = item.rect[key];
+				}
+
+				increment += item.rect.width;
+			}
+
+			return items;
+		},
+
 		setDisplay : function () {
 			var container = this.container;
 			var properties = container.properties;
 
-			var items = this.dom.items;
-			var i, j;
+			var items = this.items;
 
 			var isInline = this.testInlineBlock(properties);
 			utils.cleanWhitespace(this.dom.container);
+
+			if (isInline) {
+				this.storePositionValues(items);
+				this.applyPositioning(container, items);
+			}
 		},
 
 		render : function (settings) {
