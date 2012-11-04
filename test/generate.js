dac74d30ab2b3caeb9c7000f55ea1d72475e78d9
Richard Herrera <rich@doctyper.com>
2012-11-04 10:05:15 -0800

    Adding socket support. Generator now listens for CSS changes and re-deploys when necessary.

--- test/generate.js
+++ test/generate.js
@@ -5,22 +5,45 @@
 var fs = require("fs");
 var express = require("express");
 var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
+server.listen(9999);
+
+io.sockets.on("connection", function (socket) {
+	var css = __dirname + "/css/runner.css";
+	var flex = __dirname + "/data/flex.js";
+
+	fs.watchFile(css, function (curr, prev) {
+		socket.emit("csschange");
+	});
+
+	fs.watchFile(flex, function (curr, prev) {
+		socket.emit("datachange");
+	});
+});
 
 app.configure(function () {
 	app.use(express.bodyParser());
 	app.use("/css", express.static(__dirname + "/css"));
+	app.use("/lib", express.static(__dirname + "/lib"));
+	app.use("/data", express.static(__dirname + "/data"));
 
-	app.set('views', __dirname + '/views');
-	app.engine('html', require('ejs').renderFile);
+	app.set("views", __dirname + "/views");
+	app.engine("html", require("ejs").renderFile);
 });
 
 app.get("/properties", function (req, res) {
-	var file = fs.readFileSync(__dirname + "/flex-properties.js");
+	var file = fs.readFileSync(__dirname + "/data/flex-properties.js");
 	res.json(JSON.parse(file));
 });
 
 app.get("/generate", function (req, res) {
 	res.render("generate.html");
+});
+
+app.get("/runner", function (req, res) {
+	res.render("runner.html");
 });
 
 app.post("/flex", function (req, res) {
