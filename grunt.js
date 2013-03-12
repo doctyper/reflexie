29c061da83f1d5306f1c0ed550bfb3069184e4b1
Richard Herrera <rich@doctyper.com>
2013-03-12 11:47:17 -0700

    Re-adding Items.

--- grunt.js
+++ grunt.js
@@ -21,6 +21,7 @@ module.exports = function (grunt) {
 				"polyfill/container/alignItems",
 				"polyfill/container/alignContent",
 				"polyfill/container",
+				"polyfill/items",
 				"polyfill",
 				"exports",
 				"outro"
