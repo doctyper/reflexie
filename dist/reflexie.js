a3f19b8c967222ed9169c39218001a6653f2eecf
pci <philip.ingrey@googlemail.com>
2013-03-18 13:34:56 +0000

    (very) basis support for flex-grow

--- dist/reflexie.js
+++ dist/reflexie.js
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * Date: 3-17-2013
+ * Date: 3-18-2013
  */
 (function (window, undefined) {
 
@@ -420,7 +420,7 @@
 		}
 	};
 	
-	Flexbox.models.flexGrow = function (properties) {
+	Flexbox.models.flexGrow = function (flewGrow, properties) {
 		// Check for space, otherwise exit
 		var values = this.values,
 			container = values.container,
@@ -433,16 +433,66 @@
 
 			containerMainSize = container[mainSize],
 			containerCrossSize = container[crossSize],
-			lines = this.lines,
+			lines = this.lines;
 
-			i, j, key;
+		var utils = Flexbox.utils,
+			colArray = ["column", "column-reverse"],
+			revArray = ["row-reverse", "column-reverse"],
+			flexDirection = properties["flex-direction"],
+			isColumn = utils.assert(flexDirection, colArray),
+			isReverse = utils.assert(flexDirection, revArray);
+
+		var i, ilim, j, jlim, line, noOfItems, usedSpace,
+			availSpace, flexTotal, curr, runningDiff, dir;
+
+		for (i = 0, ilim = lines.length; i < ilim; i++) {
+			line = lines[i];
+			noOfItems = line.items.length;
+
+			// TODO Properly: calculate hypothetical cross size of each item
+			// Currently just use width (i.e. borders will currently make this wrong!)
+
+			usedSpace = 0;
+			for (j = 0; j < noOfItems; j++) {
+				usedSpace += line.items[j][mainSize];
+			}
 
 			// TODO Properly: Determine the available main and cross space for the flex items (9.2)
-			// Currently just using containerSize
-			for(key in lines)
-				console.log(lines[key]);
+			// Currently just using containerMainSize
 
-			console.log(mainSize, crossSize, containerMainSize);
+			availSpace = containerMainSize - usedSpace;
+
+			if (availSpace < 0) {
+				// Need flex-shrink rather than flex-grow
+				return properties;
+			}
+
+			flexTotal = 0;
+			for (j = 0; j < noOfItems; j++) {
+				curr = line.items[j].debug.properties["flex-grow"];
+				if (!curr || isNaN(curr) || curr < 0) {
+					curr = line.items[j].debug.properties["flex-grow"] = 0;
+				}
+				flexTotal += curr;
+			}
+
+	
+			if (flexTotal <= 0) {
+				// Nothing can grow - do nothing!
+				return properties;
+			}
+
+			// TODO: Implent min/max-width/height support
+			runningDiff = 0;
+			dir = (isReverse ?  -1 : 1);
+			for (j = 0; j < noOfItems; j++) {
+				curr = (availSpace * line.items[j].debug.properties["flex-grow"]) / flexTotal;
+				line.items[j][mainStart] += (isReverse ?  -runningDiff - curr : runningDiff);
+				line.items[j][mainSize] += curr;
+				runningDiff += curr;
+			}
+		}
+
 	};
 	Flexbox.models.flexDirection = function (direction, properties) {
 		var values = this.values,
