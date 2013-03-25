17da7af405aedbbbc79b92f7d7d5ebd844619cc1
Richard Herrera <rich@doctyper.com>
2013-03-24 19:35:17 -0700

    Moving towards manual Flexie initialization. I think it makes more sense based on the number of times I've had to edit CSS before invoking Flexie. This way, Flexie can be async and also wait for the user to be ready for it.
    - Removing DomReady.
    - Adding MicroEvent for event emitting.
    - Expose Flexie.init and Flexie.redraw properties.
    - Fire an oncomplete event when Flexie is done.

---
+++ src/event.js
@@ -0,0 +1,29 @@
+Flexbox.event = {
+	handlers : {
+		redraw : function () {
+			var options = Flexie.options;
+
+			if (typeof options.redraw === "function") {
+				options.redraw.apply(Flexie, arguments);
+			}
+		},
+
+		complete : function () {
+			var options = Flexie.options;
+
+			if (typeof options.complete === "function") {
+				options.complete.apply(Flexie, arguments);
+			}
+		}
+	},
+
+	emitter: function () {
+		var ee = new MicroEvent(),
+			handlers = Flexbox.event.handlers;
+
+		ee.bind("redraw", handlers.redraw);
+		ee.bind("complete", handlers.complete);
+
+		return ee;
+	}
+};
