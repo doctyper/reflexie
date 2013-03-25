6f03e0c787a1cceb428c8d047858ae46389d1985
Richard Herrera <rich@doctyper.com>
2013-03-24 19:27:53 -0700

    Combine functionality of align-items and align-self for both performance and clarity.

--- src/polyfill/items/alignSelf.js
+++ src/polyfill/items/alignSelf.js
@@ -3,24 +3,45 @@ Flexbox.models.alignSelf = function (alignment, properties) {
 		crossSize = this.crossSize,
 		multiplier = 1,
 		lines = this.lines,
-		i, j, k, l, line, item;
+		i, j, k, l, line, item,
+		lineRemainder;
 
 	var values = this.values;
 	var mainSize = this.mainSize;
 	var containerSize = values.container[mainSize];
 
 	var crossTotal = crossStart + "Total";
+	var lineCrossSize, m, n, cItem;
 
 	var isNotFlexWrap = properties["flex-wrap"] === "nowrap";
+	var isAlignContentStretch = properties["align-content"] === "stretch";
 
 	var alignSelf, lineSize;
-	var isAuto, isStart, isCenter, isStretch;
+	var isStart, isCenter, isStretch, isBaseline;
+
+	// Figure out remainders & max item sizes
+	var remainderSize = containerSize;
+	var currentRemainderSize;
 
 	for (i = 0, j = lines.length; i < j; i++) {
 		line = lines[i];
 
 		for (k = 0, l = line.items.length; k < l; k++) {
 			item = line.items[k];
+			line.maxItemSize = Math.max(line.maxItemSize || 0, (item[crossSize] + item.debug.inner[crossStart]) + item.debug.margin[crossTotal]);
+		}
+
+		remainderSize -= line.maxItemSize;
+	}
+
+	remainderSize /= lines.length;
+
+	for (i = 0, j = lines.length; i < j; i++) {
+		line = lines[i];
+
+		for (k = 0, l = line.items.length; k < l; k++) {
+			item = line.items[k];
+			currentRemainderSize = remainderSize;
 
 			if (!item.debug || !item.debug.properties) {
 				return;
@@ -28,26 +49,61 @@ Flexbox.models.alignSelf = function (alignment, properties) {
 
 			alignSelf = item.debug.properties["align-self"];
 
-			isAuto = alignSelf === "auto";
+			// If auto, align-self value is inherited from align-items
+			if (alignSelf === "auto") {
+				alignSelf = properties["align-items"];
+			}
+
 			isStart = alignSelf === "flex-start";
 			isCenter = alignSelf === "center";
 			isStretch = alignSelf === "stretch";
+			isBaseline = alignSelf === "baseline";
 
-			lineSize = (isNotFlexWrap) ? containerSize : line.maxItemSize;
-			lineSize -= item.debug.inner[crossStart];
-			lineSize -= item.debug.margin[crossTotal];
+			if (isStretch && isNotFlexWrap) {
+				lineRemainder = values.container[crossSize] / lines.length;
 
-			if (isStretch) {
 				if (item.debug.auto[crossSize]) {
-					item[crossSize] = lineSize;
+					if (i) {
+						item[crossStart] += (lineRemainder - item[crossSize]) * i;
+					}
+
+					item[crossSize] = (lineRemainder - item.debug.inner[crossStart] - item.debug.margin[crossTotal]);
 				}
-			} else if (!isAuto && !isStart) {
-				if (isCenter) {
-					multiplier = 0.5;
+			} else if (isStretch) {
+				lineCrossSize = 0;
+
+				for (m = 0, n = line.items.length; m < n; m++) {
+					cItem = line.items[m];
+
+					if (cItem.debug.auto[crossSize]) {
+						lineCrossSize = Math.max(lineCrossSize, (cItem[crossSize] + cItem.debug.inner[crossStart]) + cItem.debug.margin[crossTotal]);
+					}
 				}
 
-				item[crossStart] += (lineSize - item[crossSize]) * multiplier;
+				if (item.debug.auto[crossSize]) {
+					item[crossSize] = (lineCrossSize - item.debug.inner[crossStart]) - item.debug.margin[crossTotal];
+				}
 			}
+
+			// No furths if any of these apply
+			if (isStretch || isStart || isBaseline) {
+				continue;
+			}
+
+			if (isCenter) {
+				multiplier = 0.5;
+				currentRemainderSize *= 0.5;
+			}
+
+			if (!isNotFlexWrap && !isAlignContentStretch) {
+				currentRemainderSize = 0;
+			}
+
+			lineRemainder = line.maxItemSize;
+
+			// Remove margin from crossStart
+			item[crossStart] -= item.debug.margin[crossTotal] * multiplier;
+			item[crossStart] += currentRemainderSize + (lineRemainder - (item[crossSize] + item.debug.inner[crossStart])) * multiplier;
 		}
 	}
 };
